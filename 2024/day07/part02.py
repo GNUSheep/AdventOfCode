c1cc5208c9ada0d8d91db8c8ab8163020f919b5e
GNUSheep <pentet221@gmail.com>
2024-12-08 11:02:53 +0100

    Lack of time yesterday; doing day07

---
+++ 2024/day07/part02.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+def backtrack(equations, test_value, current_value, i):
+    if i == len(equations):
+        return current_value == test_value
+
+    if backtrack(equations, test_value, current_value + equations[i], i+1):
+        return True
+
+    if backtrack(equations, test_value, current_value * equations[i], i+1):
+        return True
+
+    if i > 0:
+        con_nums = int(str(current_value) + str(equations[i]))
+        if backtrack(equations, test_value, con_nums, i+1):
+            return True
+
+    return False
+    
+result = 0
+for line in stdin:
+    line_splitted = line.split()
+
+    test_value = int(line_splitted[0][:-1])
+    equations = [int(n) for n in line_splitted[1:]]
+
+    if backtrack(equations, test_value, 0, 0):
+        result += test_value
+print(result)  
+    
